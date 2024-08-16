6cb213244a14929ac8e1fb53b849fed82a1b2f1d
Uliay8 <128229025+Uliay8@users.noreply.github.com>
2024-08-16 20:28:47 +0500

    s

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:cripto_coins_list/cripto_currencies_list_app.dart';
-import 'package:cripto_coins_list/router/router.dart';
-import 'package:cripto_coins_list/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,30 +1,28 @@
 import 'package:flutter/material.dart';
 
 final darkTheme = ThemeData(
-        // primarySwatch: Colors.yellow,
-        // primaryColor: Colors.yellow,
-        // colorSchemeSeed: Colors.yellowAccent,
-        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        // dividerColor: Colors.white24,
-        appBarTheme: const AppBarTheme(
-            backgroundColor: Color.fromARGB(255, 31, 31, 31),
-            centerTitle: true,
-            titleTextStyle: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.w700)),
-        scaffoldBackgroundColor: const Color.fromARGB(255, 31, 31, 31),
-        listTileTheme: const ListTileThemeData(iconColor: Colors.white),
-        textTheme: TextTheme(
-            bodyMedium: const TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.w500,
-              fontSize: 20,
-            ),
-            labelSmall: TextStyle(
-              color: Colors.white.withOpacity(0.6),
-              fontWeight: FontWeight.w700,
-              fontSize: 14,
-            )),
-        useMaterial3: true,
-      )
+  // primarySwatch: Colors.yellow,
+  // primaryColor: Colors.yellow,
+  // colorSchemeSeed: Colors.yellowAccent,
+  // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+  // dividerColor: Colors.white24,
+  appBarTheme: const AppBarTheme(
+      backgroundColor: Color.fromARGB(255, 31, 31, 31),
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+          color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700)),
+  scaffoldBackgroundColor: const Color.fromARGB(255, 31, 31, 31),
+  listTileTheme: const ListTileThemeData(iconColor: Colors.white),
+  textTheme: TextTheme(
+      bodyMedium: const TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.w500,
+        fontSize: 20,
+      ),
+      labelSmall: TextStyle(
+        color: Colors.white.withOpacity(0.6),
+        fontWeight: FontWeight.w700,
+        fontSize: 14,
+      )),
+  useMaterial3: true,
+);
